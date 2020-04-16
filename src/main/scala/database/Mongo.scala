37be1bc610a5f810f5224d72a10186184472ae46
dinopuguh <puguhmunung16@gmail.com>
2020-04-16 10:02:53 +0700

    clear repo

--- src/main/scala/database/Mongo.scala
+++ src/main/scala/database/Mongo.scala
@@ -1,17 +1,9 @@
 package database
 
-import scala.collection.immutable.IndexedSeq
-
 import org.mongodb.scala.{MongoClient, MongoDatabase}
-import org.mongodb.scala.model.Aggregates._
-import org.mongodb.scala.model.Filters._
-import org.mongodb.scala.model.Projections._
-import org.mongodb.scala.model.Sorts._
-import org.mongodb.scala.model.Updates._
-import org.mongodb.scala.model._
 
 object Mongo {
-  val mongoClient: MongoClient = MongoClient()
+  val mongoClient: MongoClient = MongoClient("mongodb://localhost:27018")
 
   val database: MongoDatabase = mongoClient.getDatabase("kawulo")
 }
